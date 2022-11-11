daffc8826da1192da52563e906d91cde038a2aca
soniSUHANI <soni03suhani@gmail.com>
2022-11-11 19:15:16 +0530

    factorial

---
+++ for2.py
@@ -0,0 +1,13 @@
+num = int(input("Enter a number here:"))
+factorial = 1
+
+if num<0:
+    print("must be positive")
+elif num == 0:
+    print("factorial = 1")
+else:
+    for i in range(1,num +1):
+        factorial = factorial*i
+print(factorial)
+
+
