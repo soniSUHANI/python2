45f9e345f3cd431c5b7e9bf4bd372ea8c5356c4f
soniSUHANI <114800693+soniSUHANI@users.noreply.github.com>
2022-11-28 20:08:20 +0530

    Add files via upload
    tuples qns and practice

---
+++ tuples4.py
@@ -0,0 +1,17 @@
+# tuple1= (1,2,3)
+# tuple2=(3,4,5)
+# x = tuple1+tuple2
+# print(x)
+#tuple1= (10,20,30,40)
+# list1 = list(tuple1)
+# print(tuple(list1[1:3]))
+
+# tuple1=(10,20,30,40)
+# tuple2=(tuple1[1],tuple1[2])
+# print(tuple2)
+
+# tuple1=(10,20,30,40,20)
+# print(tuple1.count(20))
+num1,num2,num3,num4,num5=str(input("enter name here: ")).split(',')
+tuple = (num1,num2,num3,num4,num5)
+print(tuple)
